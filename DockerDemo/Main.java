7867587207c253a118c49ebaa0f87c4acc28bc07
ABHISHEK JADHAV <codeforfun@gmail.com>
2024-03-12 11:55:15 +0530

    Update files.

--- DockerDemo/Main.java
+++ DockerDemo/Main.java
@@ -1,5 +1,9 @@
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Abhishek : Docker Demo");
+        for(int i=1;i<=10;i++){
+            System.out.println("Count: "+i+"...10");
+            Thread.sleep(5000);
+        }
     }
 }
